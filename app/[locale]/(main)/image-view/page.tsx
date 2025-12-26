c2a90cce5c5e43952c80fb5ed1f8258015a40103
coder-HDY <d_huang@dreamarts.co.jp>
2025-12-26 09:55:59 +0800

    feat: home image

--- app/[locale]/(main)/image-view/page.tsx
+++ app/[locale]/(main)/image-view/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useEffect, useRef, useState, useCallback } from "react";
-import { motion } from "framer-motion";
+import { useState, useCallback } from "react";
 import ImageWithSkeleton from "@/components/ImageWithSkeleton";
 import useLoadMore from "@/hooks/useLoadMore";
 
@@ -54,14 +53,7 @@ export default function ImageStreamPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 max-w-[1400px] mx-auto">
       {images.map((src, idx) => (
-        // <motion.div
-        //   key={idx}
-        //   initial={{ opacity: 0, y: 20 }}
-        //   animate={{ opacity: 1, y: 0 }}
-        //   transition={{ duration: 0.3, ease: "easeOut" }}
-        // >
         <ImageWithSkeleton key={idx} src={src} alt={`Image ${idx}`} />
-        // </motion.div>
       ))}
       <div ref={loaderRef} className="h-12 col-span-full text-center">
         {loading && <span>loading...</span>}
